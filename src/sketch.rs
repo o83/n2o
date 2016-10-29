e73eb8b8bcf795adaf0b9ae1779b53201ab74ba1
Namdak Tonpa <maxim@synrc.com>
2016-10-29 03:21:45 +0300

    wip

--- src/sketch.rs
+++ src/sketch.rs
@@ -1,17 +1,32 @@
 
+// Try to keep dependency list as minimal as possible
+
 use std::collections::VecDeque;
 use abstractions::poll::Async;
-use core::ops::{FnOnce, FnMut};
-use std::io::{Error, Result};
+use std::io::Result;
+use std::time::Instant;
 
 // System Message with Custom Binary representation
 
-pub struct Message { }
+pub struct Message {
+    body: Vec<u64>,
+}
+
+pub trait Parser {}
+
+impl Parser for Message {}
 
 // For using with Tasks and Timers to select active subset
 
 pub trait Discipline {
     fn select(&mut self, u64) -> Result<Async<u64>>;
+}
+
+// List of Queues
+
+pub struct QueueManagerContext<Message> {
+    queues: Vec<QueueContext<Message>>,
+    size: u64,
 }
 
 // Queues Manager
@@ -32,8 +47,38 @@ pub struct QueueContext<Message> {
 // Queue API
 
 pub trait Queue<Message> {
-    fn push(&mut self, Message) -> Queue<Message>;
-    fn pop(&mut self) -> Message;
+    fn push(&mut self, Message) -> Result<Async<u64>>;
+    fn pop(&mut self) -> Result<Async<Message>>;
+}
+
+// Queue Linking/Topology
+
+pub struct Multicast {
+    pipes: Vec<u64>,
+}
+
+// Publish
+
+pub trait Pub<Message> {
+    fn register(&mut self, QueueContext<Message>);
+    fn publish(&mut self);
+}
+
+impl<Message> Pub<Message> for Multicast {
+    fn register(&mut self, a: QueueContext<Message>) {}
+    fn publish(&mut self) {}
+}
+
+// Subscribe
+
+pub trait Sub<Message> {
+    fn register<F>(&mut self, F) where F: Fn(Message) -> ();
+    fn subscribe(&mut self, u64);
+}
+
+impl<Message> Sub<Message> for Multicast {
+    fn register<F>(&mut self, fun: F) where F: Fn(Message) -> () {}
+    fn subscribe(&mut self, a: u64) {}
 }
 
 // Task Scheduler/Reactor
@@ -54,11 +99,11 @@ pub trait Reactor<Task>: Discipline {
 pub struct Task<Protocol, State> {
     state: Vec<u8>,
     prio: u64,
-    lambda: FnMut(Protocol, State) -> State,
+    lambda: Fn(Protocol, State) -> State,
 }
 
 pub trait Process<Protocol, State> {
-    fn send(&mut self, Protocol, State) -> State;
+    fn send(&mut self, Protocol, State) -> Result<Async<State>>;
 }
 
 // Timer Reactor Context
@@ -85,13 +130,31 @@ pub struct Timer {
 
 // Network Subsystems
 
-pub struct Network {
+pub struct NetworkContext {
     cursor: u64,
     sockets: Queue<Socket>,
+}
+
+pub struct NetworkPoll {}
+
+// X-SOCK native backend
+
+pub trait Network {
+    fn open(u8);
+    fn close();
+    fn client(i8, NetworkContext, Socket);
+    fn server(i8, NetworkContext, Socket);
+    fn create(NetworkPoll);
+    fn destroy(NetworkPoll);
+    fn add(NetworkPoll, Socket, cookie: u64);
+    fn remove(NetworkPoll, Socket);
+    fn poll(NetworkPoll, Instant, i8);
+    fn send(Socket, VecDeque<u64>, u64, sent: u64);
+    fn recv(Socket, VecDeque<u64>, u64, received: u64);
 }
 
 // Socket Context
 
 pub struct Socket {
-    buffer: Queue<Message>,
+    buffer: VecDeque<u64>,
 }
