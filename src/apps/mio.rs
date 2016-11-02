f5c2d985bd4667f07662ad84ef39525a254811d5
Namdak Tonpa <maxim@synrc.com>
2016-11-02 09:57:23 +0200

    more samples to the god of samples

---
+++ src/apps/mio.rs
@@ -0,0 +1,34 @@
+extern crate kernel;
+
+use kernel::io::*;
+use kernel::io::poll::*;
+use kernel::io::ready::*;
+use kernel::io::token::*;
+use kernel::io::options::*;
+use kernel::io::tcp::*;
+
+const SERVER: Token = Token(0);
+const CLIENT: Token = Token(1);
+
+// MIO Compatibility Sample
+
+fn main() {
+
+    let addr = "127.0.0.1:13265".parse().unwrap();
+    let server = TcpListener::bind(&addr).unwrap();
+    let sock = TcpStream::connect(&addr).unwrap();
+    let mut poll = Poll::new().unwrap();
+    let mut events = Events::with_capacity(1024);
+    poll.register(&server, SERVER, Ready::readable(), PollOpt::edge()).unwrap();
+    poll.register(&sock, CLIENT, Ready::readable(), PollOpt::edge()).unwrap();
+    loop {
+        poll.poll(&mut events, None).unwrap();
+        for event in events.iter() {
+            match event.token() {
+                  SERVER => { let _ = server.accept(); }
+                  CLIENT => { return; }
+                       _ => unreachable!(),
+            }
+        }
+    }
+}
