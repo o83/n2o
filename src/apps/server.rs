73fc34ed5a57380e1ac7978bb6af9ccce8d281d3
Namdak Tonpa <maxim@synrc.com>
2016-11-04 20:07:00 +0200

    server/console

--- src/apps/server.rs
+++ src/apps/server.rs
@@ -4,14 +4,21 @@ use std::net::SocketAddr;
 use kernel::io::poll::*;
 use kernel::io::tcp::*;
 use kernel::io::server::*;
+use kernel::io::console::Console;
+use std::thread;
 
-// Simple Network Server
+// Simple Network and Console server
 
 fn main() {
     println!("IO Server started");
-    let addr        = "127.0.0.1:8000".parse::<SocketAddr>().ok().expect("Parser Error");
-    let sock       = TcpListener::bind(&addr).ok().expect("Failed to bind address");
-    let mut poll   = Poll::new().expect("Failed to create Poll");
-    let mut server = Server::new(sock);
-    server.run(&mut poll).expect("Failed to run server");
+    let x = thread::spawn(|| {
+        let addr = "127.0.0.1:8000".parse::<SocketAddr>().ok().expect("Parser Error");
+        let sock = TcpListener::bind(&addr).ok().expect("Failed to bind address");
+        let mut poll1 = Poll::new().expect("Failed to create Poll");
+        let mut net = Server::new(sock);
+        net.run(&mut poll1).expect("Failed to run server");
+    });
+    let mut poll2 = Poll::new().expect("Failed to create Poll");
+    let mut con = Console::new();
+    con.run(&mut poll2);
 }
