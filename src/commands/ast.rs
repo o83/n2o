00015fd4aa576c101a866d564686daefac0e56d4
Namdak Tonpa <maxim@synrc.com>
2016-11-06 18:02:22 +0200

    K-parc AST

--- src/commands/ast.rs
+++ src/commands/ast.rs
@@ -1,7 +1,17 @@
 
+// K primitives: http://kparc.com/lisp.txt
+
 #[derive(Debug)]
 pub enum AST {
-    Integer(u64),
-    Symbol(String),
+    Integer(u64), Symbol(String),
     Float(f64),
+    Append, Get, Set,
+    Curry, Compose, Lambda(Box<AST>),
+    Expr, Nil,
+    CommaList(Box<AST>), ColonList(Box<AST>),
+    Cons(Box<AST>,Box<AST>), Car, Setq, Cond,
+    Map, Reduce, Min, Max,
+    Plus, Add, Mul, Div,
+    Greater, Less, Equal,
+    Length, Reverse, Member,
 }
