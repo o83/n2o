a6c572d5895e0c54c8ec7ef0bb7f25d98fbfa9d3
singaraiona <singaraiona@gmail.com>
2016-11-10 19:17:58 +0200

    Added simple K tests.

---
+++ tests/k.rs
@@ -0,0 +1,34 @@
+extern crate kernel;
+
+use kernel::commands::*;
+use kernel::commands::ast::*;
+
+#[test]
+pub fn k_ariph() {
+    assert_eq!(format!("{:?}", command::parse_Mex("1+2")),
+               "Ok(Sentence(Number(1), Plus, Number(2)))");
+
+    assert_eq!(format!("{:?}", command::parse_Mex("1+2*4")),
+               "Ok(Sentence(Sentence(Number(1), Plus, Number(2)), Times, Number(4)))");
+}
+
+#[test]
+pub fn k_list() {
+    assert_eq!(format!("{:?}", command::parse_Mex("(1;2;3;4)")),
+               "Ok(List(Cons(Number(1), Cons(Number(2), Cons(Number(3), Cons(Number(4), Nil))))))");
+}
+
+#[test]
+pub fn k_func() {
+    assert_eq!(format!("{:?}", command::parse_Mex("{x*2}[(1;2;3)]")),
+               "Ok(Call(Lambda(Nil, Sentence(Name(\"x\"), Times, Number(2))), \
+                Cons(List(Cons(Number(1), Cons(Number(2), Cons(Number(3), Nil)))), Nil)))");
+}
+
+#[test]
+pub fn k_adverb() {
+    assert_eq!(format!("{:?}", command::parse_Mex("{x+2}/(1;2;3)")),
+               "Ok(Sentence(Adverb(Over), Call(Lambda(Nil, Sentence(Name(\"x\"), Plus, \
+                Number(2))), Cons(List(Cons(Number(1), Cons(Number(2), Cons(Number(3), Nil)))), \
+                Nil)))");
+}
