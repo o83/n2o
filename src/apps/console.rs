7d8bb1d8ec98a56e02baa2176414b0362975b621
5HT <maxim@synrc.com>
2016-12-08 12:56:04 +0200

    env logger

--- src/apps/console.rs
+++ src/apps/console.rs
@@ -12,7 +12,12 @@ extern crate kernel;
 extern crate core;
 #[macro_use]
 extern crate log;
+extern crate time;
+extern crate env_logger;
 
+use env_logger::LogBuilder;
+use log::{LogRecord, LogLevelFilter};
+use std::env;
 use kernel::io::poll::*;
 use kernel::reactors::console::*;
 use std::boxed::*;
@@ -23,7 +28,7 @@ use kernel::args::*;
 fn main() {
     let mut poll = Poll::new().expect("Failed to create Poll");
     let mut c = Console::new();
-
+    setup_logger();
     let mut p = Parser::new();
     if let Ok(init) = p.get("init", true) {
         match File::open(init.unwrap()) {
@@ -39,4 +44,28 @@ fn main() {
     }
 
     c.run(&mut poll);
+}
+
+// logger initial setup
+fn setup_logger() {
+    let format = |record: &LogRecord| {
+        let t = time::now();
+        format!("{}\t[{},{:03} {}:{}]\t--> {}",
+                record.level(),
+                time::strftime("%Y-%m-%d %H:%M:%S", &t).unwrap(),
+                t.tm_nsec / 1000_000,
+                record.location().__module_path,
+                record.location().__line,
+                record.args()
+               )
+    };
+
+    let mut builder = LogBuilder::new();
+    //builder.format(format).filter(Some("console"), LogLevelFilter::Debug);
+
+    if env::var("RUST_LOG").is_ok() {
+        builder.parse(&env::var("RUST_LOG").unwrap());
+    }
+
+    builder.init().unwrap();
 }
