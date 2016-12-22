b03bf77477a1a80e3468bbb878bcbf36967ef748
5HT <maxim@synrc.com>
2016-12-22 19:50:48 +0200

    ws client

--- src/apps/wserver.rs
+++ src/apps/wserver.rs
@@ -4,10 +4,10 @@ use std::net::SocketAddr;
 use kernel::io::ws::*;
 
 fn main() {
-    let addr = "127.0.0.1:8000".parse::<SocketAddr>().ok().expect("Parser Error");
+    let addr = "0.0.0.0:9001".parse::<SocketAddr>().ok().expect("Parser Error");
     let mut ws = WsServer::new(&addr);
     ws.listen(|(s, m)| {
         println!("Message: {:?}", m);
-        s.write_message(&[1, 2, 3]);
+        s.write_message(&[131, 106]);
     });
 }
