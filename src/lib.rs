df2b912a7802cd7537aa80d4fb49c22411b3bf1d
5HT <maxim@synrc.com>
2017-01-13 12:48:57 +0200

    clean

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,6 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 //#![allow(unused_imports)]
-#![allow(unused_must_use)]
 #![allow(unused_unsafe)]
 #![allow(unused_variables)]
 #![allow(unused_features)]
