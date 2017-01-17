808476a543c7a6a0740a57483f82cc18a44e8f1f
Namdak Tonpa <maxim@synrc.com>
2017-01-17 02:56:30 +0200

    tests should be equal to implementation

--- tests/core_int.rs
+++ tests/core_int.rs
@@ -4,27 +4,23 @@
 // $ cat core_int.s | grep mul
 
 #[target_feature = "+avx"]
-pub fn mul_array_float<'a>(x: &mut [f64], y: &[f64]) {
-    for (xi, &yi) in x.iter_mut().zip(y.iter()) {
-        *xi *= yi;
-    }
+pub fn mul_array_float<'a>(x: &mut[f64], y: &[f64]) {
+    let n: Vec<f64> = x.iter().zip(y).map(|(d,c)|d*c).collect();
+    println!("{:?}",n);
 }
 
 #[target_feature = "+avx"]
 pub fn mul_array<'a>(x: &mut [u64], y: &[u64]) {
-    for (xi, &yi) in x.iter_mut().zip(y.iter()) {
-        *xi *= yi;
-    }
+    let n: Vec<u64> = x.iter().zip(y).map(|(d,c)|d*c).collect();
+    println!("{:?}",n);
 }
 
 fn main() {
-    let mut fa = vec![10.0, 1.0, 4.0, 9.0, 10.0, 1.0, 4.0, 9.0, 10.0, 1.0, 4.0, 9.0, 10.0, 1.0, 4.0, 9.0];
-    let fb     = vec![10.0, 2.0, 6.0, 2.0, 10.0, 1.0, 4.0, 9.0, 10.0, 2.0, 6.0, 2.0, 10.0, 1.0, 4.0, 9.0];
-    let mut  a = vec![10, 1, 4, 9, 10, 1, 4, 9, 10, 1, 4, 9, 10, 1, 4, 9];
-    let  b     = vec![10, 2, 6, 2, 10, 1, 4, 9, 10, 2, 6, 2, 10, 1, 4, 9];
-    mul_array(&mut a, &b);
-    mul_array_float(&mut fa, &fb);
-    println!("{:?}", fa);
-    println!("{:?}", a);
+    let mut fa = vec![0.0, 1.0, 4.0, 9.0, 0.0, 1.0, 4.0, 9.0, 0.0, 1.0, 4.0, 9.0, 0.0, 1.0, 4.0, 9.0];
+    let fb     = vec![0.0, 2.0, 6.0, 0.0, 0.0, 1.0, 4.0, 9.0, 0.0, 2.0, 6.0, 0.0, 0.0, 1.0, 4.0, 9.0];
+    let mut a  = vec![0, 1, 4, 9, 0, 1, 4, 9, 0, 1, 4, 9, 0, 1, 4, 9];
+    let b      = vec![0, 2, 6, 0, 0, 1, 4, 9, 0, 1, 4, 9, 0, 1, 4, 9];
+    mul_array_float(fa.as_mut_slice(), fb.as_slice());
+    mul_array(a.as_mut_slice(), b.as_slice());
+    println!("{:?} {:?}", a, fa);
 }
-
