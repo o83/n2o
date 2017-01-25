8185772438bd3571ce331c0aa0ac07a26c3fcfec
Namdak Tonpa <maxim@synrc.com>
2017-01-26 00:05:03 +0200

    remove boot and init

--- src/apps/legacy.rs
+++
@@ -1,8 +0,0 @@
-#[macro_use]
-extern crate kernel;
-use kernel::reactors::init;
-
-fn main() {
-    let mut h = init::host();
-    h.borrow_mut().run();
-}
--- src/reactors/mod.rs
+++ src/reactors/mod.rs
@@ -1,11 +1,9 @@
 pub mod console;
 pub mod selector;
 pub mod system;
-pub mod boot;
 pub mod cpstask;
 pub mod scheduler;
 pub mod task;
 pub mod ws;
-pub mod init;
 pub mod job;
 pub mod fusion;
