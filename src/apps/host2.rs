90157a461ed216dc9cd85ad3adee8156a0049aa5
lseugene <ls.eugene@gmail.com>
2017-01-25 19:05:40 +0200

    host 2 app

---
+++ src/apps/host2.rs
@@ -0,0 +1,79 @@
+#[macro_use]
+extern crate kernel;
+use std::{thread, time};
+use kernel::reactors::scheduler::Scheduler;
+use kernel::sys;
+use std::sync::Arc;
+use std::io::{self, BufReader, BufRead};
+use kernel::reactors::console::Console;
+use kernel::reactors::selector::{Selector, Async, Pool};
+use kernel::reactors::system::IO;
+
+
+fn construct_scheds<'a>(sched_num: usize) -> Vec<Scheduler<'a>> {
+    let mut scheds: Vec<Scheduler<'a>> = Vec::new();
+    for i in 0..sched_num {
+        let mut sched = Scheduler::with_channel(i);
+        for s in &mut scheds {
+            s.bus.subscribers.push(sched.bus.publisher.subscribe());
+            sched.bus.subscribers.push(s.bus.publisher.subscribe());
+        }
+        scheds.push(sched);
+    }
+    scheds
+}
+
+
+fn park_scheds<'a>(mut scheds: Vec<Scheduler<'a>>) {
+    let sz = scheds.len();
+    for id in 0..sz {
+        if let Some(mut s) = scheds.pop() {
+            unsafe {
+                spawn_on(id, || {
+                    s.run();
+                });
+            }
+        }
+    }
+}
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<Self>) {
+        (*self)()
+    }
+}
+
+// Like `std::thread::spawn`, but without the closure bounds.
+pub unsafe fn spawn_on<'a, F>(id: usize, f: F) -> thread::JoinHandle<()>
+    where F: FnOnce() + Send + 'a
+{
+    use std::mem;
+    let closure: Box<FnBox + 'a> = Box::new(f);
+    let closure: Box<FnBox + Send> = mem::transmute(closure);
+    println!("spawn on core_id {:?}", id);
+    thread::Builder::new()
+        .name(format!("core_{}", id))
+        .spawn(move || {
+            sys::set_affinity(1 << id);
+            closure.call_box()
+        })
+        .expect("Can't spawn new thread!")
+}
+
+fn io_loop(io: IO) {}
+
+fn main() {
+    park_scheds(construct_scheds(5));
+    let mut io = IO::new();
+    io.spawn(Selector::Rx(Console::new()));
+    loop {
+        match io.poll() {
+            Async::Ready((_, p)) => println!("console: {:?}", p),
+            Async::NotReady => (),
+        }
+    }
+}
