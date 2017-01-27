9a2474e0b07c7b4f59cb654cf38398c5c380c1cf
lseugene <ls.eugene@gmail.com>
2017-01-27 12:13:15 +0200

    turn off avx in test

--- tests/core_int.rs
+++ tests/core_int.rs
@@ -3,23 +3,23 @@
 // $ rustc -C opt-level=3 --emit asm core_int.rs
 // $ cat core_int.s | grep mul
 
-#[target_feature = "+avx"]
-pub fn mul_array_float<'a>(x: &mut[f64], y: &[f64]) {
-    let n: Vec<f64> = x.iter().zip(y).map(|(d,c)|d*c).collect();
-    println!("{:?}",n);
+//#[target_feature = "+avx"]
+pub fn mul_array_float<'a>(x: &mut [f64], y: &[f64]) {
+    let n: Vec<f64> = x.iter().zip(y).map(|(d, c)| d * c).collect();
+    println!("{:?}", n);
 }
 
-#[target_feature = "+avx"]
+//#[target_feature = "+avx"]
 pub fn mul_array<'a>(x: &mut [u64], y: &[u64]) {
-    let n: Vec<u64> = x.iter().zip(y).map(|(d,c)|d*c).collect();
-    println!("{:?}",n);
+    let n: Vec<u64> = x.iter().zip(y).map(|(d, c)| d * c).collect();
+    println!("{:?}", n);
 }
 
 fn main() {
     let mut fa = vec![0.0, 1.0, 4.0, 9.0, 0.0, 1.0, 4.0, 9.0, 0.0, 1.0, 4.0, 9.0, 0.0, 1.0, 4.0, 9.0];
-    let fb     = vec![0.0, 2.0, 6.0, 0.0, 0.0, 1.0, 4.0, 9.0, 0.0, 2.0, 6.0, 0.0, 0.0, 1.0, 4.0, 9.0];
-    let mut a  = vec![0, 1, 4, 9, 0, 1, 4, 9, 0, 1, 4, 9, 0, 1, 4, 9];
-    let b      = vec![0, 2, 6, 0, 0, 1, 4, 9, 0, 1, 4, 9, 0, 1, 4, 9];
+    let fb = vec![0.0, 2.0, 6.0, 0.0, 0.0, 1.0, 4.0, 9.0, 0.0, 2.0, 6.0, 0.0, 0.0, 1.0, 4.0, 9.0];
+    let mut a = vec![0, 1, 4, 9, 0, 1, 4, 9, 0, 1, 4, 9, 0, 1, 4, 9];
+    let b = vec![0, 2, 6, 0, 0, 1, 4, 9, 0, 1, 4, 9, 0, 1, 4, 9];
     mul_array_float(fa.as_mut_slice(), fb.as_slice());
     mul_array(a.as_mut_slice(), b.as_slice());
     println!("{:?} {:?}", a, fa);
